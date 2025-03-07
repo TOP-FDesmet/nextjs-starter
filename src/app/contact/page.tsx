993a38348096d4815eac09b1ba3597150a5b062b
TOP-FDesmet <florian.desmet@topchretien.com>
2025-03-07 16:37:19 +0100

    useRouter

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function Contact() {
-  return <h1>Ma page de contact</h1>;
+  const router = useRouter();
+
+  const handleSendContactForm = () => {
+    console.log("Formulaire de contact envoyé !");
+    router.push("/");
+  };
+
+  return (
+    <>
+      <h1>Ma page de contact</h1>
+      <button onClick={handleSendContactForm}>Valider le formulaire</button>
+    </>
+  );
 }
